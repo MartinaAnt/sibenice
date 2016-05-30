d279fd6cac9c13a157f56f04df5e68c585475423
Martina <antalovova@gmail.com>
2016-05-30 13:31:32 +0200

    fungující doplňování písmen chybí obrázky

--- sibenice.py
+++ sibenice.py
@@ -1,17 +1,33 @@
 import random
+import obrazky
+import sys
 
-seznam = ("zajíc", "robča", "zlato")
+seznam = ["koření", "robča", "zlato"]
 vybrane_slovo = random.choice(seznam)
-pole = len(vybrane_slovo)*"_"
 
 
-pokusy = 0
+def hrac (pole):
+    pokusy = 0
+    pismeno = input("Jaké tipuješ písmeno?: ")
 
-pismeno = input("Jaké tipuješ písmeno?: ")
-if pismeno in vybrane_slovo:
-    return pole[:vybrane_slovo.index(pismeno)] + pismeno + pole[vybrane_slovo.index(pismeno) + 1:]
-else:
-    pokusy=pokusy+1
-    print("Toto písmeno tu není")
-    print(obrazek)
-    print pole
+    if pismeno in vybrane_slovo:
+        return pole[:vybrane_slovo.index(pismeno)] + pismeno + pole[vybrane_slovo.index(pismeno) + 1:]
+
+    else:
+        pokusy += 1
+        print("Toto písmeno tu není")
+        return pole
+
+
+def vyhodnoceni():
+    pole = len(vybrane_slovo)*"_"
+    print(pole)
+    while True:
+        if "_" in pole:
+            pole = hrac(pole)
+            print(pole)
+        else:
+         print ("Ty jsi ale holka šikovná! :-) ")
+         break
+
+#vyhodnoceni()
