80e59d6e9eaa2dbf496ea60e6c57761b4d22f363
Martina <antalovova@gmail.com>
2016-05-30 14:58:24 +0200

    fungují obrázky

--- sibenice.py
+++ sibenice.py
@@ -1,33 +1,37 @@
 import random
 import obrazky
-import sys
 
-seznam = ["koření", "robča", "zlato"]
-vybrane_slovo = random.choice(seznam)
-
+seznam_slov = ["koření", "robča", "zlato"]
+vybrane_slovo = random.choice(seznam_slov)
+seznam_pokusu = []
+pouzita = []
 
 def hrac (pole):
-    pokusy = 0
     pismeno = input("Jaké tipuješ písmeno?: ")
-
     if pismeno in vybrane_slovo:
         return pole[:vybrane_slovo.index(pismeno)] + pismeno + pole[vybrane_slovo.index(pismeno) + 1:]
-
     else:
-        pokusy += 1
-        print("Toto písmeno tu není")
+        if pismeno in pouzita:
+            print("Fakt tam není")
+        else:
+            print("Toto písmeno tu není")
+        seznam_pokusu.append(len(seznam_pokusu))
+        obrazky.seznam[seznam_pokusu[-1]]()
+        pouzita.append(pismeno)
         return pole
 
-
-def vyhodnoceni():
+def hra():
     pole = len(vybrane_slovo)*"_"
     print(pole)
     while True:
-        if "_" in pole:
+        if len(seznam_pokusu) > 8:
+            print("Game Over")
+            break
+        elif "_" in pole:
             pole = hrac(pole)
             print(pole)
         else:
-         print ("Ty jsi ale holka šikovná! :-) ")
-         break
+            print ("Ty jsi ale holka šikovná! :-) ")
+            break
 
-#vyhodnoceni()
+hra()
